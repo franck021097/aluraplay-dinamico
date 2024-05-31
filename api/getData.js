f12c310a2c7ff4dddab663cd876df22e0742fd7b
Francisco Castro <franck_021097@hotmail.com>
2024-05-30 18:27:23 -0600

    getData.js

---
+++ api/getData.js
@@ -0,0 +1,18 @@
+import { Pool } from 'pg';
+
+const pool = new Pool({
+  connectionString: process.env.POSTGRES_URL,
+});
+
+export default async function handler(req, res) {
+  try {
+    const client = await pool.connect();
+    const result = await client.query('SELECT * FROM aluraplay');
+    const rows = result.rows;
+    client.release();
+    res.status(200).json(rows);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Error al obtener datos' });
+  }
+}
