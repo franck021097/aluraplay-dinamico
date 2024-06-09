f07adc2bb12d96896a899131be620e32d599b48a
Francisco Castro <franck_021097@hotmail.com>
2024-06-09 17:01:39 -0600

    respaldo mac

--- js/crearVideo.js
+++ js/crearVideo.js
@@ -11,19 +11,10 @@ async function crearVideo(evento){
     const imagen = document.querySelector("[data-imagen]").value;
 
     const descripcion = Math.floor(Math.random()*(9000)+1000);
-    const respuesta = await conectaAPI.enviarVideo(titulo,descripcion,url,imagen);
+    await conectaAPI.enviarVideo(titulo,descripcion,url,imagen);
 
-    try{
-        
-        if(respuesta.ok){
-            window.location.href ="../pages/envio-concluido.html";
-        }else{
-            throw respuesta;
-        }
-    }catch(e){
-        console.log(e.status);
-        formulario.innerHTML = `<h2 class="message_error">Ups, algo salió mal. <br><br> Error: ${e}</h2>`;
-    }
+    window.location.href ="../pages/envio-concluido.html";
+    
 }
 
 
